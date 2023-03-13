a92311c0f772ded19c1375011dc04f020855edda
Aliaksandr Bychyk <aliaksandr_bychyk@epam.com>
2023-03-13 23:17:34 +0100

    feat: add NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>404</h2>
+      </>
+    );
+  }
+}
+
+export default NotFound;
