6ac05b57e4a6c17e0d3b5937b0421b18d8fb75ef
Aliaksandr Bychyk <aliaksandr_bychyk@epam.com>
2023-03-13 23:18:37 +0100

    feat: add AboutUs page

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About Us</h2>
+      </>
+    );
+  }
+}
+
+export default AboutUs;
