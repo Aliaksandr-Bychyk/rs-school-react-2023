e2a1ab3f367c8c9f50930c7c2e0ff2c3a486e42a
Aliaksandr Bychyk <aliaksandr_bychyk@epam.com>
2023-03-13 23:27:03 +0100

    feat: add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/Main';
+import NotFound from './pages/NotFound';
+import AboutUs from './pages/AboutUs';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />,
+  },
+]);
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Main />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
