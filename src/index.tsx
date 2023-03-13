ae91b730550625a0eb9f6754a4a78bfb48e998aa
Aliaksandr Bychyk <aliaksandr_bychyk@epam.com>
2023-03-13 23:16:17 +0100

    feat: add Main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Main from './pages/Main';
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    <Main />
   </React.StrictMode>
 );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Header</h2>
+      </>
+    );
+  }
+}
+
+export default Main;
