1ba42cbab1bd4d058739cbfca9a4172c19b4acd8
Aliaksandr <sanya55bychyk@gmail.com>
2023-03-30 12:06:05 +0200

    feat: rewrite Main page to FC

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,37 +1,24 @@
+import React, { FC, useState } from 'react';
 import cards from '../../data/cards';
-import React, { Component } from 'react';
 import Card from '../../components/Card/Card';
 import Header from '../../components/Header/Header';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import ICardData from '../../interfaces/ICardData';
 import './Main.css';
 
-interface IMainState {
-  items: ICardData[];
-}
-
-class Main extends Component<object, IMainState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      items: cards,
-    };
-  }
-
-  render() {
-    const items = this.state.items as unknown[];
-    return (
-      <>
-        <Header title="Main" />
-        <SearchBar />
-        <div className="post-container">
-          {items.map((el, index) => (
-            <Card key={index} data={el as ICardData} />
-          ))}
-        </div>
-      </>
-    );
-  }
-}
+const Main: FC = () => {
+  const [items] = useState<ICardData[]>(cards);
+  return (
+    <>
+      <Header title="Main" />
+      <SearchBar />
+      <div className="post-container">
+        {items.map((el, index) => (
+          <Card key={index} data={el as ICardData} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Main;
