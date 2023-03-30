3a1af94fd96605e87b9da0131c13e9ed734ab5c2
Aliaksandr <sanya55bychyk@gmail.com>
2023-03-30 15:07:46 +0200

    feat: rewrite Popup to FC

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './Popup.css';
 
 interface IPopupProps {
   isSuccessful: boolean;
 }
 
-class Popup extends React.Component<IPopupProps> {
-  constructor(props: IPopupProps) {
-    super(props);
-  }
-  render() {
-    return this.props.isSuccessful ? (
-      <div className="popup-container popup-success">Success! The data has been saved!</div>
-    ) : (
-      <div className="popup-container popup-fail">Fail! Please check!</div>
-    );
-  }
-}
+const Popup: FC<IPopupProps> = ({ isSuccessful }) => {
+  return isSuccessful ? (
+    <div className="popup-container popup-success">Success! The data has been saved!</div>
+  ) : (
+    <div className="popup-container popup-fail">Fail! Please check!</div>
+  );
+};
 
 export default Popup;
