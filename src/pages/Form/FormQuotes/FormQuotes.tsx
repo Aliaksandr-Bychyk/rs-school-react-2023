c455b4bd5023143d2c62880c3152a5085127e836
Aliaksandr <sanya55bychyk@gmail.com>
2023-03-30 15:32:50 +0200

    feat: rewrite FormQuotes to FC

--- src/pages/Form/FormQuotes/FormQuotes.tsx
+++ src/pages/Form/FormQuotes/FormQuotes.tsx
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import './FormQuotes.css';
 
 interface IFormQuotesProps {
   formRef: React.RefObject<HTMLSelectElement>;
 }
 
-class FormQuotes extends Component<IFormQuotesProps> {
-  constructor(props: IFormQuotesProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="form-quotes-container">
-        <label>
-          Favorite quote:&nbsp;
-          <select ref={this.props.formRef}>
-            <option value="alan">Alan Shepard</option>
-            <option value="neli">Neil Armstrong</option>
-            <option value="sally">Sally Ride</option>
-            <option value="doug">Doug Hurley</option>
-            <option value="john">John Swigert</option>
-          </select>
-        </label>
-      </div>
-    );
-  }
-}
+const FormQuotes: FC<IFormQuotesProps> = ({ formRef }) => {
+  return (
+    <div className="form-quotes-container">
+      <label>
+        Favorite quote:&nbsp;
+        <select ref={formRef}>
+          <option value="alan">Alan Shepard</option>
+          <option value="neli">Neil Armstrong</option>
+          <option value="sally">Sally Ride</option>
+          <option value="doug">Doug Hurley</option>
+          <option value="john">John Swigert</option>
+        </select>
+      </label>
+    </div>
+  );
+};
 
 export default FormQuotes;
