27181fa7c67c806db969afdbf438d7310666d1d4
Aliaksandr <sanya55bychyk@gmail.com>
2023-03-30 12:13:30 +0200

    feat: rewrite Router to FC

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AboutUs from '../pages/AboutUs/AboutUs';
 import Main from '../pages/Main/Main';
@@ -27,14 +27,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-class Router extends React.Component {
-  render() {
-    return (
-      <>
-        <RouterProvider router={router} />
-      </>
-    );
-  }
-}
+const Router: FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default Router;
