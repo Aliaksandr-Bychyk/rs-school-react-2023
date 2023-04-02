08315cd57b818eb0896c089e7b1510110455b322
Aliaksandr <sanya55bychyk@gmail.com>
2023-04-03 00:35:35 +0200

    feat: add test to increase coverage

--- src/pages/Form/Form.test.tsx
+++ src/pages/Form/Form.test.tsx
@@ -1,16 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import Form from './Form';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-describe('Form', () => {
-  it('Renders', () => {
+// describe('Form', () => {
+//   it('Renders', () => {
+//     render(
+//       <BrowserRouter>
+//         <Form />
+//       </BrowserRouter>
+//     );
+//     expect(screen.getAllByText(/Form/i)).toBeDefined();
+//   });
+//   // it('Button click', () => {
+//   //   const { getByText } = render(
+//   //     <BrowserRouter>
+//   //       <Form />
+//   //     </BrowserRouter>
+//   //   );
+//   //   const button = getByText('Submit');
+//   //   // ex
+//   // });
+// });
+describe('Accordion', () => {
+  beforeEach(() => {
     render(
       <BrowserRouter>
         <Form />
       </BrowserRouter>
     );
-    expect(screen.getAllByText(/Form/i)).toBeDefined();
+  });
+
+  test('should show title all the time', () => {
+    expect(screen.getByText(/Form/i)).toBeInTheDocument();
+  });
+
+  test('should not show the popup at the start', () => {
+    expect(screen.queryByText(/Fail! Please check!/i)).not.toBeInTheDocument();
+  });
+
+  test('should show the popup on accordion click', async () => {
+    const title = screen.getByText(/Submit/i);
+    fireEvent.click(title);
+
+    expect(await screen.findByText(/Fail! Please check!/i)).toBeInTheDocument();
   });
 });
