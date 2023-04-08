d4cda0ef65e522b8fe70fd6854577826d9caddb3
Aliaksandr <sanya55bychyk@gmail.com>
2023-04-08 13:30:29 +0200

    feat: add default image

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,7 +10,17 @@ const Card: FC<ICardProps> = ({ data }) => {
   const { title, imageUrl, newsSite, publishedAt } = data;
   return (
     <div className="card-container">
-      <img className="card-image" src={imageUrl} alt={title} width="300" />
+      <img
+        className="card-image"
+        src={imageUrl}
+        alt={title}
+        width="300"
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src =
+            'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png';
+        }}
+      />
       <p className="card-title">{title}</p>
       <div className="card-info">
         <span className="card-info-author">{newsSite}</span>
