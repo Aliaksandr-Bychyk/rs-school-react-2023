bd07392e9651afbe7709bc3978744dca8a261ddd
Aliaksandr <sanya55bychyk@gmail.com>
2023-04-16 23:47:12 +0200

    refactor: setupTest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,15 +2,10 @@ import matches from '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom/extend-expect';
 import { expect } from 'vitest';
 import server from './mocks/server';
-import { store } from './redux/store';
-import { spaceFlightNewsApi } from './redux/apiSlice';
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 
-afterEach(() => {
-  server.resetHandlers();
-  store.dispatch(spaceFlightNewsApi.util.resetApiState());
-});
+afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
 
