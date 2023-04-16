3419320f72f30c45245771eac21b865e6648612c
Aliaksandr <sanya55bychyk@gmail.com>
2023-04-16 23:45:21 +0200

    feat: remove unnecessary lines

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,15 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import matches from '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom/extend-expect';
 import { expect } from 'vitest';
 import server from './mocks/server';
 import { store } from './redux/store';
 import { spaceFlightNewsApi } from './redux/apiSlice';
-
-global.fetch = fetch;
-global.Headers = Headers;
-global.Request = Request;
-global.Response = Response;
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 
